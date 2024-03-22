69a0a0b63a3a64906fea418605980c93c0e325bf
Kyle M. Lang <kylelang86@gmail.com>
2024-03-22 20:16:58 +0100

    working on implementing some unit tests to check behavior when analyzing data with problematic variable names

---
+++ tests/test-generator-subroutines.R
@@ -0,0 +1,131 @@
+### Title:    Subroutines for Generating Unit Tests
+### Author:   Kyle M. Lang (Functions extracted from ./generate-tests.R and adapted)
+### Created:  2024-03-22
+### Modified: 2024-03-22
+
+###--------------------------------------------------------------------------------------------------------------------------###
+
+.getOptionsOneModel <- function(k, parms) {
+  options <- jaspTools::analysisOptions("ClassicProcess")
+  options[["dependent"]]  <- parms$vars$y
+  options[["factors"]]    <- parms$vars$factor
+
+  covs <- parms$vars$numeric
+  if(k < 82) covs <- head(covs, -1)
+
+  options[["covariates"]] <- covs
+
+  options[["moderationProbes"]] <- list(
+    list(
+      "probePercentile" = 16.0,
+      "value" = "16"
+    ),
+    list(
+      "probePercentile" = 50.0,
+      "value" = "50"
+    ),
+    list(
+      "probePercentile" = 84.0,
+      "value" = "84"
+    )
+  )
+
+  options[["emulation"]] <- "lavaan"
+  options[["errorCalculationMethod"]] <- "standard"
+  options[["estimator"]] <- "default"
+  options[["naAction"]] <- "fiml"
+
+  options[["statisticalPathPlotsCovariances"]] <- TRUE
+  options[["statisticalPathPlotsResidualVariances"]] <- TRUE
+  options[["pathPlotsLegend"]] <- TRUE
+  options[["pathPlotsColor"]] <- TRUE
+  options[["pathPlotsColorPalette"]] <- "colorblind"
+
+  options[["processModels"]] <- list(
+    list(
+      "conceptualPathPlot" = TRUE,
+      "independentCovariances" = TRUE,
+      "inputType" = "inputVariables",
+      "mediationEffects" = TRUE,
+      "mediatorCovariances" = TRUE,
+      "modelNumber" = k,
+      "modelNumberCovariates" = list(),
+      "modelNumberIndependent" = "",
+      "modelNumberMediators" = list(),
+      "modelNumberModeratorW" = "",
+      "modelNumberModeratorZ" = "",
+      "name" = paste("Model", k),
+      "pathCoefficients" = TRUE,
+	    "intercepts" = FALSE,
+      "processRelationships" = list(),
+      "residualCovariances" = TRUE,
+      "statisticalPathPlot" = TRUE,
+      "totalEffects" = TRUE,
+      "localTests" = FALSE,
+      "localTestType" = "cis",
+      "localTestBootstrap" = FALSE,
+      "localTestBootstrapSamples" = 1000
+    )
+  )
+  return(options)
+}
+
+###--------------------------------------------------------------------------------------------------------------------------###
+
+.replaceVariables <- function(x, map) {
+  out <- map[names(map) %in% x] |> as.character()
+  ifelse(length(out) > 0, out, x)
+}
+
+###--------------------------------------------------------------------------------------------------------------------------###
+
+.addProcessRelationships <- function(k, parms) {
+  customModels <- parms$customModels
+  ifelse(is.null(customModels),
+         jaspProcess:::.procGetHardCodedModel(k, 2),
+         customModels[[k]]) |> 
+  lapply(lapply, .replaceVariables, map = parms$map)
+}
+
+###--------------------------------------------------------------------------------------------------------------------------###
+
+.polishCapturedCode <- function(out) {
+  out |>
+    gsub("Now rendering a plot with name: conceptPathPlot", "", x = _) |>
+    gsub("Now rendering a plot with name: statPathPlot", "", x = _) |>
+    gsub("could not find an old plot", "", x = _) |>
+    gsub("Did not store jaspResults", "", x = _) |>
+    gsub("Created Write Seal for jaspResults at: ''", "", x = _) |>
+    gsub(" analysisOptions", " jaspTools::analysisOptions", x = _) |>
+    gsub(" runAnalysis", " jaspTools::runAnalysis", x = _) |>
+    gsub("[\n]{4,}", "", x = _) |>
+    gsub("test_that\\(\".+?\", \\{", "", x = _) |>
+    gsub("\\}\\)", "", x = _)
+}
+
+###--------------------------------------------------------------------------------------------------------------------------###
+
+.captureTestCode <- function(k, opts, parms) {
+  out <- capture.output(jaspTools::runAnalysis("ClassicProcess", dataset = parms$data, options = opts, makeTests = TRUE)) |>
+    paste(collapse = "\n") |>
+    try(silent = TRUE)
+    
+  if (!inherits(out, "try-error")) {
+    type <- parms$type
+    out <- out |>
+      .polishCapturedCode() |>
+      gsub("conceptual-path-plot", paste("conceptual-path-plot", type, k, sep = "-"), x = _) |>
+      gsub("statistical-path-plot", paste("statistical-path-plot", type, k, sep = "-"), x = _)
+
+    out <- paste0("test_that(\"Test that model number ", k, " - ", type, " works\", {\n", out, "\n})")
+  } 
+  out
+}
+
+###--------------------------------------------------------------------------------------------------------------------------###
+
+makeTestString <- function(k, parms) { 
+  opts <- .getOptionsOneModel(k, parms)
+  opts$processModels[[1]][["processRelationships"]] <- .addProcessRelationships(k, parms)
+  .captureTestCode(k, opts, parms)
+}
