1d35e808c460efe5c261280418c82ad4fe37c722
maltelueken <malte_lueken@arcor.de>
2024-05-30 15:14:23 +0200

    Always recompute model graphs to avoid state dependency issues

--- R/classicProcess.R
+++ R/classicProcess.R
@@ -151,11 +151,11 @@ ClassicProcess <- function(jaspResults, dataset = NULL, options) {
     modelOptions <- options[["processModels"]][[i]]
     modelName <- modelOptions[["name"]]
 
-    if (is.null(modelsContainer[[modelName]][["graph"]])) {
-      graph <- try(.procModelGraphSingleModel(options[["processModels"]][[i]], globalDependent = options[["dependent"]]))
-      state <- createJaspState(object = graph)
-      modelsContainer[[modelName]][["graph"]] <- state
-    }
+    # We need to regenerate the model graph everytime because it gets
+    # modified in a later step
+    graph <- try(.procModelGraphSingleModel(options[["processModels"]][[i]], globalDependent = options[["dependent"]]))
+    state <- createJaspState(object = graph)
+    modelsContainer[[modelName]][["graph"]] <- state
   }
 }
 
@@ -188,7 +188,7 @@ ClassicProcess <- function(jaspResults, dataset = NULL, options) {
     type            <- path[["processType"       ]]
     processVariable <- path[["processVariable"   ]]
 
-    # Encode variable names if the model is defined through the "Variables" dialog
+    # Encode variable names if the model is defined through the "Paths" dialog
     if (inputVariables) {
       dependent       <- encodeColNames(dependent)
       independent     <- encodeColNames(independent)
