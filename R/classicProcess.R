87b7c9c16d0845036955c9f45c8e8d06415587f4
maltelueken <malte_lueken@arcor.de>
2025-07-14 19:22:36 +0200

    Add better error message for internal errors

--- R/classicProcess.R
+++ R/classicProcess.R
@@ -1490,7 +1490,7 @@ ClassicProcess <- function(jaspResults, dataset = NULL, options) {
 .procResultsFitModel <- function(container, dataset, options, modelOptions) {
   # Check if graph has error message
   if (!.procCheckGraph(container[["graph"]]$object) && jaspBase::isTryError(container[["graph"]]$object)) {
-    return(.procEstimationMsg(container[["graph"]]$object))
+    return(.procEstimationMsg())
   }
 
   # Should model be fitted?
@@ -1516,7 +1516,7 @@ ClassicProcess <- function(jaspResults, dataset = NULL, options) {
   ))
 
   if (jaspBase::isTryError(fittedModel)) {
-    return(.procLavaanMsg(fittedModel))
+    return(.procEstimationMsg())
   }
 
   if (doFit && is.null(lavaan::vcov(fittedModel))) {
@@ -1824,7 +1824,7 @@ ClassicProcess <- function(jaspResults, dataset = NULL, options) {
   } else if (jaspBase:::isTryError(procResult)) {
     container$setError(jaspBase::.extractErrorMessage(procResult))
   } else if (!inherits(procResult, "lavaan")) {
-    container$setError(.procEstimationMsg(.procModelCheckMsg()))
+    container$setError(.procEstimationMsg())
   }
 }
 
@@ -3038,9 +3038,7 @@ ClassicProcess <- function(jaspResults, dataset = NULL, options) {
 
 .procHayesModelMsg <- function(modelName, modelNumber) gettextf("%1$s: Hayes configuration %2$s not implemented", modelName, modelNumber)
 
-.procEstimationMsg <- function(graph) gettextf("Estimation failed: %s", jaspBase::.extractErrorMessage(graph))
-
-.procLavaanMsg <- function(fittedModel) gettextf("Estimation failed: %s", gsub("lavaan ERROR:", "", jaspBase::.extractErrorMessage(fittedModel)))
+.procEstimationMsg <- function() gettextf("Estimation failed because of an internal error.")
 
 .procFimlMsg <- function() gettext("Full Information Maximum Likelihood estimation only available with 'ML' or 'Auto' estimators. Please choose a different estimator or option for missing value handling.")
 
